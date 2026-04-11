384249f2fb0ebfb21ed7db32cd6af31583e09259
Hey83 <pixertps@gmail.com>
2026-04-12 03:35:14 +0800

    Delete Task2/heapsort.py

--- Task2/heapsort.py
+++
@@ -1,8 +0,0 @@
-def heap_sort(arr):
-    # Sorts a list in ascending order using Heap Sort algorithm.
-    
-    # 1. Builds a min-heap from the input array.
-    # 2. Repeatedly extracts the minimum element (root) and places it at the end.
-    # 3. Restores the heap property on the reduced heap.
-    
-    pass
